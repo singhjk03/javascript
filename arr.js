25672ea1cd3aad0a1920bb82ae011d63c96f7588
Jayant Singh <jayantsingh1024@gmail.com>
2023-05-13 13:26:05 +0530

    THis is javascript file

---
+++ arr.js
@@ -0,0 +1,19 @@
+let arr=[12,23,34,34,45,56,56];
+console.log(arr);
+
+for(var i=0;i<arr.length ;i++){
+    console.log(arr[i]);
+
+    for(var j=i+1;j<arr.length;j++){
+        if(arr[i]==arr[j]){
+            for(var k=j;k<arr.length-1;k++){
+                arr[k]=arr[k+1];
+            }
+            arr.length--;
+            j--;
+        }
+    }
+}
+for(i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
